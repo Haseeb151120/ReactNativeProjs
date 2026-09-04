99eb2c912dbdca143bc7a32ea485d29762ace9d9
Haseeb Ahsan <haseeb46ahsan@gmail.com>
2026-09-04 11:45:09 +0500

    LabTask 03/09/2026

---
+++ task1.js
@@ -0,0 +1,9 @@
+let students =[
+    {Name: "Haseeb" , RegNo : "23-ARID-3679", Department: "CS", Grade: "A", Marks: 10},
+    {Name: "Ali", RegNo: "23-ARID-3775", Department: "SE", Grade: "B" , Marks: 8},
+    {Name: "Ahmed", RegNo: "23-ARID-3768", Department: "AI", Grade: "B", Marks: 7},
+    {Name: "Saad", RegNo: "23-ARID-4753", Department: "CS",  Grade: "D", Marks: 4 }
+
+];
+
+console.log(students);
